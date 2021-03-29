e1170b2d54ced544ba8fe2edecc2bd416f650c26
Swann Pincet <swann.pincet@gmail.com>
2021-03-29 10:55:54 +0200

    init + initial 8 players list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+const players = [
+  "playera",
+  "playerb",
+  "playerc",
+  "playerd",
+  "playere",
+  "playerf",
+  "playerg",
+  "playerh"
+]
+
+console.log(players.length)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello
     </div>
   );
 }
