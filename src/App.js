f34129b5af2811b4606577b14372fc0d6728cc54
Swann Pincet <swann.pincet@gmail.com>
2021-03-30 12:38:53 +0200

    for development only - created a function that creates a random playerbase

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import TournamentExact from './TournamentExact'
 
 import './App.css';
 
+//helper to create a player DB > for development only
+const randomPlayerDB = (count) => {
+  const players = []
+  for (let i = 0; i < count; i++) {
+    let id = ""
+    for (let j = 0;j< 5;j++) {
+      id += Math.floor(Math.random() * 10)
+    }
+    players.push(id)
+  }
+  return players
+}
 
 function App() {
 
-  const players = [
-    "playera",
-    "playerb",
-    "playerc",
-    "playerd",
-    "playere",
-    "playerf",
-    "playerg",
-    "playerh"
-  ]
-
   return (
     <div>
-      <TournamentExact players={players}/>
+      <TournamentExact players={randomPlayerDB(32)}/>
     </div>
   );
 }
